d8e3b085a9da269a36e4512fc38bab7930756e73
siratl <elishubax@gmail.com>
2019-03-21 16:03:06 -0700

    Initialized knex file and ran migrations and seeds on the database. 500 User models created!

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,8 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
-    client: 'postgresql',
+    client: 'sqlite3',
     connection: {
       filename: './database/reminders.db3',
     },
@@ -13,7 +12,7 @@ module.exports = {
     },
     seeds: {
       directory: './database/seeds',
-    }
+    },
   },
 
   testing: {
@@ -27,39 +26,22 @@ module.exports = {
     },
     seeds: {
       directory: './database/seeds',
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
   },
 
   production: {
-    client: 'postgresql',
+    client: 'pg',
     connection: {
       database: 'my_db',
-      user:     'username',
-      password: 'password'
+      user: 'username',
+      password: 'password',
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+      tableName: 'knex_migrations',
+    },
+  },
 };
