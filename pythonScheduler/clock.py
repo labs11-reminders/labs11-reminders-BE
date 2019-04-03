1ff56f203626f31944ecaf54acbc13be7673422c
Katie Cleary <katiecleary@airmaster.lan>
2019-04-03 13:30:51 -0400

    removed any nonworking code from python program

--- pythonScheduler/clock.py
+++ pythonScheduler/clock.py
@@ -12,24 +12,6 @@ from twilio.rest import Client
 
 #db = sqlite3.connect('../database/reminders.db3') - local testing 
 
-#DB connection - put in both files after one help post said it needed to be in the worker file 
-url = urlparse.urlparse(os.environ['DATABASE_URL'])
-dbname = url.path[1:]
-user = url.username
-password = url.password
-host = url.hostname
-port = url.port
-
-db = psycopg2.connect(
-            dbname=dbname,
-            user=user,
-            password=password,
-            host=host,
-            port=port
-            )
-
-db.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-
 
 
 #env variables for worker funtions 
@@ -47,10 +29,10 @@ client = Client(accountSid,authToken)
 @sched.scheduled_job('interval', minutes=2)
 #changed interval to two minutes for testing purposes
 def timed_job():
-    worker.row_factory()
-    worker.create_messages()
-    worker.requires_send()
-    worker.send_message(client)
+    #worker.row_factory()
+    #worker.create_messages()
+    #worker.requires_send()
+    #worker.send_message(client)
     print('This job is run every five minutes.')
 
 
