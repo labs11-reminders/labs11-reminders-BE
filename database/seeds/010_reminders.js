f748da3dd078ad2779c43d90aa1820c6d1c87861
RachelKolk <rach.m.obrien@gmail.com>
2019-04-08 08:53:55 -0400

    Cannot truncate a table with Foreign Key x2

--- database/seeds/010_reminders.js
+++ database/seeds/010_reminders.js
@@ -1,7 +1,7 @@
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('reminders')
-    .truncate()
+    // .truncate()
     .then(function() {
       // Inserts seed entries
       return knex('reminders').insert([
